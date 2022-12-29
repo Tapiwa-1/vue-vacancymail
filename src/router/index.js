12a0474c044fd39e4378ba9ac4a27e650c0a0963
Tapiwa Motsi <tmotsi10@gmail.com>
2022-12-29 19:29:12 +0200

    recreation

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/HomeView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'homeMain',
-      component: ()=> import('../views/Frontend/HomeMain.vue'),
-      children:[{
-        path:'/',
-        name: 'home',
-        component: ()=> import('../views/Frontend/HomeView.vue')
-      },
-    ]
+      name: 'Home',
+      component: Home,
+     
     },
-    {
-      path:'/employer-login',
-      name:'employerLogin',
-      component: ()=> import('../views/Backend/LoginView.vue'),
-    }
   ]
 })
 
